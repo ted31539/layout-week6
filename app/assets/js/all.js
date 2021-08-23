5c389c248ef03cd1bedbd7068ee6944f8f64ea90
Ted <Ted31539@gmail.com>
2021-08-23 20:40:12 +0800

    Update 2021-08-23T12:37:30.187Z

--- app/assets/js/all.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-  console.log('Hello Bootstrap5');
-});
-
-// Initialize Swiper
-var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,
-  spaceBetween: 30,
-});
---
+++ assets/js/all.js
@@ -0,0 +1,11 @@
+"use strict";
+
+$(function () {
+  console.log('Hello Bootstrap5');
+}); // Initialize Swiper
+
+var swiper = new Swiper(".mySwiper", {
+  slidesPerView: 3,
+  spaceBetween: 30
+});
+//# sourceMappingURL=all.js.map
